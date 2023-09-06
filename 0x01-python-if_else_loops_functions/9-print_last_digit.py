7d82591d3d76033fbcc2b15137a67eae93ba0ca7
okibe onmeje <okibeogomola@gmail.com>
2023-09-05 21:39:23 -0700

    add 10-add.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,10 +4,10 @@
 def print_last_digit(number):
     if number < 0:
         number = number * -1
-        ld =  number % 10
+        ld = number % 10
         print(ld, end="")
         return ld
     else:
-        ld =  number % 10
+        ld = number % 10
         print(ld, end="")
         return ld
