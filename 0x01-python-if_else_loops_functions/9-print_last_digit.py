3d1dfe6400e5a655a50276e4e2a9e56bb98b6e16
okibe onmeje <okibeogomola@gmail.com>
2023-09-05 21:31:17 -0700

    add 9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def print_last_digit(number):
+    if number < 0:
+        number = number * -1
+        ld =  number % 10
+        print(ld, end="")
+        return ld
+    else:
+        ld =  number % 10
+        print(ld, end="")
+        return ld
