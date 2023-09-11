997d39dada3f35598872267f8b654ed792dbb3a5
Okibe Onmeje <Okibeogomola@gmail.com>
2023-09-11 18:54:30 +0100

    fix handle None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,6 @@ def print_reversed_list_integer(my_list=[]):
     '''
         description: prints a list in reverse
     '''
-    if len(my_list) == 0:
-        return
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    if my_list is not None:
+        for i in range(len(my_list) - 1, -1, -1):
+            print("{:d}".format(my_list[i]))
