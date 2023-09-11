1ef9c85c6ba5ae7e9fd0a2169038146abd2980f4
Okibe Onmeje <Okibeogomola@gmail.com>
2023-09-11 17:46:45 +0100

    fix prevent decimal formatiing

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -6,4 +6,4 @@ def print_reversed_list_integer(my_list=[]):
     list_cpy = my_list[:]
     list_cpy.reverse()
     for i in range(len(list_cpy)):
-        print("{:d}".format(list_cpy[i]))
+        print("{}".format(list_cpy[i]))
