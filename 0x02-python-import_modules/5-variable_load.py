31e982a74288c6fbfbc077716da50d1085177c73
okibe onmeje <okibeogomola@gmail.com>
2023-09-12 08:48:49 -0700

    add 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if '__' not in i:
-            print(i)
+    from variable_load_5 import a
+    print(a)
