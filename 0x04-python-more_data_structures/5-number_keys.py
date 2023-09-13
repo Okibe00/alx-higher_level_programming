56364b6da1649dd454d6faea7ab7e631a28bf6dd
Okibe Onmeje <Okibeogomola@gmail.com>
2023-09-13 19:09:09 +0100

    add 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    '''
+        description: returns the number of keys in a dictionary
+    '''
+    num = 0
+    for i in a_dictionary:
+        num = num + 1
+    return num
