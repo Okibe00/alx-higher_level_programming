d655c82221222bc6e912faa81e30bfd1fa78ccb2
Okibe Onmeje <Okibeogomola@gmail.com>
2023-09-25 17:22:21 +0100

    add 100-safe-print_integer.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     '''
         description: prints an integer
@@ -6,5 +9,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
-        print({})
+    except Exception as ec:
+        print("Exception: ", ec, file=sys.stderr)
+        return False
