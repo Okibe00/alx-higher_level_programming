e4f51c4f697d9ac7dedb5d5508f8b54cf807f8d7
Okibe Onmeje <Okibeogomola@gmail.com>
2023-09-25 16:38:40 +0100

    add 4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    '''
+        description: divides element by element 2 list
+    '''
+    res_list = list()
+    for i in range(list_length):
+        try:
+            res_list.append(my_list_1[i] / my_list_2[i])
+        except TypeError:
+            res_list.append(0)
+            print("wrong type")
+        except ZeroDivisionError:
+            res_list.append(0)
+            print("division by 0")
+        except IndexError:
+            res_list.append(0)
+            print("out of range")
+        finally:
+            continue
+    return res_list
