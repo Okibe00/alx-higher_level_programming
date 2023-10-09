c127dd39bdd048d38f995dcc11efcaf6bd46af44
Okibe Onmeje <Okibeogomola@gmail.com>
2023-10-10 00:05:49 +0100

    add 6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+modules creates a class
+'''
+
+
+class BaseGeometry:
+    '''
+    empty class
+    '''
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
