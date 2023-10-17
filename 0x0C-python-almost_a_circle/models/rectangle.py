b6c3accfd99022c3877603d048cea2bb2783c5e7
Okibe Onmeje <Okibeogomola@gmail.com>
2023-10-17 12:58:00 +0100

    fix error handling for non int value

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,10 +13,10 @@ class Rectangle(Base):
             initialization method
         '''
 
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     def to_dictionary(self):
