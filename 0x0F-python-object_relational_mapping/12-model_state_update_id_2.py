ff49f8e8ff77efc171d1eeaa9b2b83634e9e8c6a
Okibe Onmeje <Okibeogomola@gmail.com>
2023-12-18 16:59:35 +0100

    add 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''
+Module: returns states with letter a
+'''
+if __name__ == '__main__':
+    from model_state import Base, State
+    import sys
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
+    url = f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}'
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.id == 2).all()
+    query[0].name = 'New Mexico'
+    session.commit()
+    session.close()
