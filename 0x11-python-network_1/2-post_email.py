5de201ba9917a10c6fe12068de36780443d2c430
Okibe Onmeje <Okibeogomola@gmail.com>
2024-02-18 17:35:01 +0100

    add url encoding

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,8 +5,15 @@ post an email address
 
 if __name__ == "__main__":
     import urllib.request
+    import urllib.parse
     import sys
     url = sys.argv[1]
     email = sys.argv[2]
-    with urllib.request.urlopen(url, email) as rs:
+    data = {
+        'email': email
+    }
+    data = urllib.parse.urlencode(data)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as rs:
         print(rs.read().decode('utf-8'))
