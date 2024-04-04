60da76a7e7125444045f00047c180b8335545076
Okibe Onmeje <Okibeogomola@gmail.com>
2024-04-05 00:36:36 +0100

    9-script.js

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,10 @@
+/** Write a JavaScript script that fetches from https://hellosalut.stefanbohacek.dev/?lang=fr and displays the value of hello from that fetch in the HTML tag DIV#hello.
+
+The translation of “hello” must be displayed in the HTML tag DIV#hello
+You can’t use document.querySelector to select the HTML tag
+You must use the JQuery API
+Your script must work when it is imported from the <head> tag
+Please test with this HTML file in your browser: */
+$.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function (data, statusCode) {
+  $('#hello').text(data.hello);
+});
